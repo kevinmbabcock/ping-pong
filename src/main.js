38d5a89348482e1f9cf27563e7fb1c19d4691710
Kevin M Babcock <kevinmbabcock@icloud.com>
2020-04-04 19:18:45 -0700

    fixed form submission

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import { pingPong } from './ping-pong';
 import './styles.css';
 
 $(document).ready(function() {
-  $('ping-pong-form').submit(function(event) {
+  $('#ping-pong-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
     var output = pingPong(goal);
     output.forEach(function(element) {
       $('#solution').append("<li>" + element + "</li>");
-      console.log("test");
     });
   });
 });
